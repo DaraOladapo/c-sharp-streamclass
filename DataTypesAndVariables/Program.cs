56d90d86156b3e886ddbda86b4b3d029bea936d8
Dara Oladapo <daroaladapo@outlook.com>
2020-04-22 21:57:52 +0100

    Lesson -01

---
+++ DataTypesAndVariables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataTypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generic Variable
+            //var UserInput=Console.ReadLine();
+            //Console.WriteLine($"You typed: {UserInput} which is a string.");
+            //numbers
+            //int MyLuckyNumber=12;
+            //double MyLuckyDouble=12.34;
+           //Console.WriteLine($"{MyLuckyNumber} is of data type {MyLuckyNumber.GetType()}.");
+             //Console.WriteLine($"{MyLuckyDouble} is of data type {MyLuckyDouble.GetType()}.");
+
+             //strings and characters
+            //  char MyChar='C';
+            // Console.WriteLine($"{MyChar} is of data type {MyChar.GetType()}.");
+             string MyString="Dara Oladapo";
+            Console.WriteLine($"{MyString} is of data type {MyString.GetType()}.");
+
+
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var UserInput=Console.ReadLine();
+            Console.WriteLine($"You typed: {UserInput}");
+        }
+    }
+}
